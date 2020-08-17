d15326fb7476efce915a6259c15b41054fcb46d1
Sandy Jurek <sandyjurek@quickenloans.com>
2020-08-17 19:41:04 -0400

    change to Product.cs in order to check uploading

--- GroupBMidtermPOS/Product.cs
+++ GroupBMidtermPOS/Product.cs
@@ -1,7 +1,10 @@
-﻿namespace GroupBMidtermPOS
+﻿using System.Collections.Generic;
+
+namespace GroupBMidtermPOS
 {
     public class Product
     {
-        
+        string List<stuff>(string product)
+            new List 
     }
 }
