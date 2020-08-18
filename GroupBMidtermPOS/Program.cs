83c6e317c9973fc459eda29aa9c3a1dde159339d
Bernard Polynice <bepoly.nice@gmail.com>
2020-08-17 21:39:20 -0400

    Bernard up to enter payment type

--- GroupBMidtermPOS/Program.cs
+++ GroupBMidtermPOS/Program.cs
@@ -15,16 +15,39 @@ namespace GroupBMidtermPOS
             Console.WriteLine("Menu: Choose an Item");
 
             Register register = new Register(); //open a new Register
-            Console.WriteLine(register.productListFile); //write out the list of products 1 thru end of list
+            foreach (var product in register.createTemporaryListOfProductsForDemoOnly)
+            {
+                Console.WriteLine($"{product} "); //write out the list of products 1 thru end of list
+            }
             var userItem = Console.ReadLine();
 
             Console.WriteLine("Enter Quantity:");
-            var userItemQuantity = Console.ReadLine();
+            var takeUserQuantity = int.TryParse(Console.ReadLine(), out int userItemQuantity); // take user user's quantity
+            if (!takeUserQuantity)
+            {
+                Console.WriteLine("Something went wrong");
+            }
+            Console.WriteLine("Subtotal: ");
+            //items customer selected
+            //price of items * quantity = total
 
+            Console.WriteLine("Would you like to continue? (Y/N)");
+            var continueYesNo = Console.ReadLine().ToLower();
 
+            Console.WriteLine("Are you ready to check out? (Y/N) ");
+            var checkOutYesNo = Console.ReadLine().ToLower();
 
+            Console.WriteLine("Order Summary: ");
+            Console.WriteLine($"Quantity {quantity} Item{item}Price {price}");//quantity-item-price for each item. may need for each statement
+            Console.WriteLine($"Subtotal ${subtotal}");//subtotal
+            Console.WriteLine($"Tax ${tax}");//tax
+            //total
 
-
+            Console.WriteLine("Enter Payment Type: ");// working with the enums
+            Console.WriteLine("1. Cash");
+            Console.WriteLine("2. Credit/Debit Card");
+            Console.WriteLine("3. Check");
+            var paymentType = Console.ReadLine();
 
 
         }
