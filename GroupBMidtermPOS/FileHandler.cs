a368359b822314f7ce0668bd0056578502cd269c
Sandy Jurek <sandyjurek@quickenloans.com>
2020-08-17 21:21:55 -0400

    Updates to FileHandler.cs

--- GroupBMidtermPOS/FileHandler.cs
+++ GroupBMidtermPOS/FileHandler.cs
@@ -1,26 +1,63 @@
-﻿namespace GroupBMidtermPOS
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GroupBMidtermPOS
 {
     public class FileHandler
     {
+        //check if files exists, if not, create it
         public static void CreateFile(string productListFile)
         {
             if (!File.Exists(productListFile))
                 File.Create(productListFile);
         }
 
-        public static void WriteToFile(string productItemName)
+        //Modifying, or writing, to file
+        public static void WriteToFile(string productListFile, bool canAppend, List<string> linesOfInput = null)
         {
-            writer.WriteLine;
+            using (StreamWriter writer = new StreamWriter(productListFile, canAppend))
+            {
+                if (linesOfInput == null)
+                {
+                    writer.WriteLine(" the list goes in here somehow");
+                }
+                else
+                {
+                    foreach (var line in linesOfInput)
+                    {
+                        List<string> fileLines = File.ReadAllLines("products.txt").ToList();
+                        var line = reader.ReadLine();
+                        Console.WriteLine(line);
+                        writer.WriteLine(line);
+                    }
+                }
+            }
         }
-    }
 
-    public static void ReadFromFile(string productAddItem)
-    {
-        using (StreamReader reader = new StreamReader(productAddItem))
+        //Reading from file
+        public static void ReadFromFile(string productAddName)
         {
-            List<string> fileLines = File.ReadAllLines("products.txt").ToList();
-            var line = reader.ReadLine();
-            Console.WriteLine(line);
+            using (StreamReader reader = new StreamReader(productAddItem))
+            {
+                List<string> fileLines = File.ReadAllLines("products.txt").ToList();
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+                //        using (StreamReader reader = new StreamReader(productAddItem))
+                //        {
+                //            List<string> fileLines = File.ReadAllLines("products.txt").ToList();
+                //            var line = reader.ReadLine();
+                //            Console.WriteLine(line);
+                //        }
+            
+
+        //Deleting from file
+        public static void DeleteFile(string fileName)
+        {
+            File.Delete(fileName);
         }
     }
 }
