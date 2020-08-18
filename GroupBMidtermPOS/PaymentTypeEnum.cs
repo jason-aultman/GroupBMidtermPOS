9937eaa032ac988809fe0eb843e7732da080e985
Sandy Jurek <sandyjurek@quickenloans.com>
2020-08-17 20:19:07 -0400

    Added PaymentTypeEnum file.

---
+++ GroupBMidtermPOS/PaymentTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupBMidtermPOS
+{ 
+    public enum PaymentTypeEnum
+    {
+        Cash,
+        Check,
+        Credit Card,
+    }
+}
