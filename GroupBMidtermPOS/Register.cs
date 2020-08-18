ce64ef2da470a072e1dd81f822d3a371aff91baf
jason-aultman <jason.aultman@gmail.com>
2020-08-17 20:09:05 -0400

    Added temporary listOfProducts and GetSubtotalNoTax() method

--- GroupBMidtermPOS/Register.cs
+++ GroupBMidtermPOS/Register.cs
@@ -1,7 +1,42 @@
-﻿namespace GroupBMidtermPOS
+﻿using System.Collections.Generic;
+
+namespace GroupBMidtermPOS
 {
     public class Register
     {
-        //This is assigned to Jason
+        public const double TAXRATE = .06;
+        public double NumberOrdered { get; set; }
+        List<Product> listOfProducts = new List<Product>();
+
+        public List<Product> createTemporaryListOfProductsForDemoOnly(List<Product>listOfProducts)
+        {
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            listOfProducts.Add(new Product());
+            return listOfProducts;
+
+        }
+
+        public double GetSubtotalNoTax()
+        {
+          //write subtotal work here
+          return 0.0;
+        }
+        
+        public double TakePaymentCash(double cashAmount, double saleAmount)
+        {
+            return cashAmount - saleAmount;
+        }
+        
+        
     }
 }
