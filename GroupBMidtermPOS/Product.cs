efb434bcdcf241b33fd7d139af40906361cc90b4
jason-aultman <jason.aultman@gmail.com>
2020-08-19 20:55:08 -0400

    add ProductCatagoryEnum() to Product.cs

--- GroupBMidtermPOS/Product.cs
+++ GroupBMidtermPOS/Product.cs
@@ -12,7 +12,7 @@ namespace GroupBMidtermPOS
         public double Price { get; set; }
         public int ProductNumber { get; set; }
 
-       
+
         public Product()
         {
             NumberOfProducts++;
@@ -20,14 +20,36 @@ namespace GroupBMidtermPOS
 
         }
 
-        //not sure how to handle the enum for ProductCategory here
+        
         public Product(int productNumber, string name, string productCategory, double price, string description)
         {
             Name = name;
-            //ProductCategory = productCategory;
+            ProductCategory = ProductCategoryEnum(productCategory);
             Description = description;
             Price = price;
             ProductNumber = productNumber;
+
+        }
+
+        public ProductCategoryEnum ProductCategoryEnum(string productCatagory)
+        {
+            if (productCatagory == "Plush Toys")
+            {
+                return GroupBMidtermPOS.ProductCategoryEnum.PlushToys;
+            }
+            else if (productCatagory == "Learning Tools")
+            {
+                return GroupBMidtermPOS.ProductCategoryEnum.LearningTools;
+            }
+            else if (productCatagory == "Dolls")
+            {
+                return GroupBMidtermPOS.ProductCategoryEnum.Dolls;
+            }
+            else if (productCatagory == "Accessories")
+            {
+                return (GroupBMidtermPOS.ProductCategoryEnum.Accessories);
+            }
+            else return GroupBMidtermPOS.ProductCategoryEnum.Games;
         }
     }
 }
