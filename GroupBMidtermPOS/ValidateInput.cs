40a7dfb43d293c08f57d698318471d13a3566988
Bernard Polynice <bepoly.nice@gmail.com>
2020-08-20 21:09:17 -0400

    ask continue to shop

---
+++ GroupBMidtermPOS/ValidateInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupBMidtermPOS
+{
+    public static class ValidateInput
+    {
+        public static bool GetIsInteger(string userInputNumber)
+        {
+            return false;
+        }
+        public static bool CheckYesNo(string userInputCharacter)
+        {
+            return false;
+        }
+    }
+}
