d90ec09281187fa9189ba83733f03b41025fdff6
Sandy Jurek <sandyjurek@quickenloans.com>
2020-08-26 19:03:37 -0400

    my file path

--- GroupBMidtermPOS/Register.cs
+++ GroupBMidtermPOS/Register.cs
@@ -13,7 +13,9 @@ namespace GroupBMidtermPOS
         public double NumberOrdered { get; set; }
         public List<Product> listOfProducts;
         public List<Product> CurrentOrder= new List<Product>();
-        private string filePath = @"C:\Users\bepol\source\repos\GroupBMidtermPOS\GroupBMidtermPOS\Inventory.csv";
+        private string filePath = @"/inventory.csv";
+
+// C:\Users\bepol\source\repos\GroupBMidtermPOS\GroupBMidtermPOS\Inventory.csv";
         
         public double TotalSales  { get; set; }
         public Register()
